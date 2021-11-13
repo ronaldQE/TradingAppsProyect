2fe5d1d9867b60ebb9ab108e924e25e1538663c8
ronald quispe espinoza <r24.espinoza@gmail.com>
2021-11-13 13:13:14 -0400

    Menu configuarado inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'business-plan',
     pathMatch: 'full'
   },
+  // {
+  //   path: 'folder/:id',
+  //   loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  // },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'business-plan',
+    loadChildren: () => import('./business-plan/business-plan.module').then( m => m.BusinessPlanPageModule)
+  },
+  {
+    path: 'budget',
+    loadChildren: () => import('./budget/budget.module').then( m => m.BudgetPageModule)
   }
 ];
 
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,19 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss'],
 })
-export class AppComponent {
+export class MenuComponent implements OnInit {
   public appPages = [
     { title: 'Plan de negocios', url: '/folder/Plan de negocios', icon: 'bar-chart' },
     { title: 'Información de la Empresa', url: '/folder/Información de la Empresa', icon: 'business' },
     { title: 'Historial de estimaciones', url: '/folder/Historial de estimaciones', icon: 'albums' },
     { title: 'Salir', url: '/folder/Archived', icon: 'exit' },
-    // { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    // { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
   ];
-  //public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+
+  constructor() { }
+
+  ngOnInit() {}
+
 }
