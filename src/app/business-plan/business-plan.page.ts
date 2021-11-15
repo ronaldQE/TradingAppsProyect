3f4e50984cd795979ab346790055c85b7396b5dd
ronald quispe espinoza <r24.espinoza@gmail.com>
2021-11-15 10:44:45 -0400

    calculo de detalle

--- src/app/business-plan/business-plan.page.ts
+++ src/app/business-plan/business-plan.page.ts
@@ -37,6 +37,9 @@ export class BusinessPlanPage implements OnInit {
     totalProyecto: 0
   }
 
+  public cOperativo:number=0;
+  public cInversion:number=0;
+  public efectivoTotal:number=0;
 
   constructor(
     private router: Router,
@@ -58,7 +61,19 @@ export class BusinessPlanPage implements OnInit {
   getBudget(){
     this.db.getCollection<Budget>('/Estimaciones/estimicion-1/presupuesto').subscribe( (data)=>{
       this.budget = data;
-
+      if (data.otros == undefined){
+        this.budget.otros = 0;
+      }
+      if (data.efectivo == undefined){
+        this.budget.efectivo = 0;
+      }
+      if (data.banco == undefined){
+        this.budget.banco = 0;
+      }
+      this.budgetSummary.aportePropio = this.budget.efectivo + this.budget.banco+ this.budget.otros+this.operatingCapital.manoObraEmprendedor
+      this.budgetSummary.totalProyecto=this.budgetSummary.planInversion+this.budgetSummary.aportePropio-this.budget.efectivo
+      this.budgetSummary.montoFinanciar=this.budgetSummary.totalProyecto-this.budgetSummary.aportePropio
+      this.efectivoTotal = this.budget.efectivo + this.budget.banco+ this.budget.otros
     },
     (error:any) => {
       console.log(`Error: ${error}`);
@@ -69,6 +84,27 @@ export class BusinessPlanPage implements OnInit {
   getOperatingCapital(){
     this.db.getCollection<OperatingCapital>('/Estimaciones/estimicion-1/capital-operativo').subscribe( (data)=>{
       this.operatingCapital = data;
+      if(data.alquiler == undefined){
+        this.operatingCapital.alquiler=0;
+      }
+      if(data.manoObra == undefined){
+        this.operatingCapital.manoObra=0;
+      }
+      if(data.manoObraEmprendedor == undefined){
+        this.operatingCapital.manoObraEmprendedor=0;
+      }
+      if(data.promociones == undefined){
+        this.operatingCapital.promociones=0;
+      }
+      if(data.serviciosBasicos == undefined){
+        this.operatingCapital.serviciosBasicos=0;
+      }
+      this.budgetSummary.aportePropio = this.budget.efectivo + this.budget.banco+ this.budget.otros+this.operatingCapital.manoObraEmprendedor
+      let totalGastosOperativos = this.operatingCapital.alquiler+this.operatingCapital.manoObra+this.operatingCapital.serviciosBasicos
+      this.cOperativo = totalGastosOperativos + this.operatingCapital.promociones
+      this.budgetSummary.planInversion=this.cOperativo+this.cInversion
+      this.budgetSummary.totalProyecto=this.budgetSummary.planInversion+this.budgetSummary.aportePropio-this.efectivoTotal
+      this.budgetSummary.montoFinanciar=this.budgetSummary.totalProyecto-this.budgetSummary.aportePropio
 
     },
     (error:any) => {
@@ -81,7 +117,19 @@ export class BusinessPlanPage implements OnInit {
   getInvestmentCapital(){
     this.db.getCollection<InvestmentCapital>('/Estimaciones/estimicion-1/capital-de-inversion').subscribe( (data)=>{
       this.investmentCapital = data;
-
+      if(data.consultoria == undefined){
+        this.investmentCapital.consultoria=0;
+      }
+      if(data.equipamientoOficina == undefined){
+        this.investmentCapital.equipamientoOficina=0;
+      }
+      if(data.equipoComputo == undefined){
+        this.investmentCapital.equipoComputo=0;
+      }
+      this.cInversion = this.investmentCapital.consultoria+this.investmentCapital.equipamientoOficina+this.investmentCapital.equipoComputo
+      this.budgetSummary.planInversion=this.cOperativo+this.cInversion
+      this.budgetSummary.totalProyecto=this.budgetSummary.planInversion+this.budgetSummary.aportePropio-this.efectivoTotal
+      this.budgetSummary.montoFinanciar=this.budgetSummary.totalProyecto-this.budgetSummary.aportePropio
     },
     (error:any) => {
       console.log(`Error: ${error}`);
