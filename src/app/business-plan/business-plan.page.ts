998cc3e277c9884a4e0a4199e7f1fd4f6c8db28b
ronald quispe espinoza <r24.espinoza@gmail.com>
2021-11-15 01:39:46 -0400

    Recuperacion de Presupuestro

--- src/app/business-plan/business-plan.page.ts
+++ src/app/business-plan/business-plan.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { serviceDataBase } from '../services/services-database';
+
+import { Budget } from '../models/interfaces';
+
+
 
 @Component({
   selector: 'app-business-plan',
@@ -8,17 +13,38 @@ import { Router } from '@angular/router';
 })
 export class BusinessPlanPage implements OnInit {
 
+  public budget: Budget = {
+    efectivo: 0,
+    banco: 0,
+    otros: 0
+  }
   constructor(
     private router: Router,
+    public db: serviceDataBase
 
-  ) { }
+  ) {
+
+  }
 
   ngOnInit() {
+    this.getBudget();
+    console.log(this.budget);
   }
   navigateTo(path: String) {
     this.router.navigate([path]);
   }
+  getBudget(){
+    this.db.getCollection<Budget>('/Estimaciones/estimicion-1/presupuesto').subscribe( (data)=>{
+      this.budget = data;
+      console.log(data)
+      console.log(this.budget)
+    },
+    (error:any) => {
+      console.log(`Error: ${error}`);
 
+    }
+    )
+  }
 
 
 }
--- src/app/services/services-database.ts
+++ src/app/services/services-database.ts
@@ -7,10 +7,17 @@ import { Injectable } from '@angular/core';
 
 export class serviceDataBase{
     constructor(public database: AngularFireDatabase) {}
+
     actualizarDatos<tipo>(data: tipo,enlace: string,coleccion: string){
         this.database.list(enlace).set(coleccion,data);
     }
+
     recuperarDato(enlace: string){
         return this.database.list(enlace);
+    }
+
+    getCollection<tipo>(path:string){
+      const collection = this.database.object<tipo>(path);
+      return collection.valueChanges();
     }
 }
