9b29b85d0760ea8f155cb5e8c1dabce4ae98d455
jhosep tinta <jtinta86@gmail.com>
2021-11-29 17:59:31 -0400

    tabla mensual con ingresos incluidos

--- src/app/components/monthly-flow/monthly-flow.component.ts
+++ src/app/components/monthly-flow/monthly-flow.component.ts
@@ -15,9 +15,12 @@ export class MonthlyFlowComponent implements OnInit {
   public initialBalance:number = 0;
   public planPagosVariado = new Cuota();
   public cuotas = [];
+  public costoVenta = [];
+  public venta = [];
   constructor(public db: serviceDataBase) { }
 
   ngOnInit() {
+    this.calculateCostOfSale();
     this.calculateDues();
     this.getTotalOperatingCosts();
     this.getBudgetSummary();
@@ -88,8 +91,8 @@ export class MonthlyFlowComponent implements OnInit {
 
   calculateNextMonthlyFlow(month){
     this.currentMonthlyFlow.saldoInicial = this.previousMonthFlow.flujoAcumulado;
-    this.currentMonthlyFlow.ingresos = 46000;
-    this.currentMonthlyFlow.costoProduccion = 1359;
+    this.currentMonthlyFlow.ingresos = this.venta[month];
+    this.currentMonthlyFlow.costoProduccion = this.costoVenta[month];
     this.currentMonthlyFlow.utilidadBruta = this.currentMonthlyFlow.ingresos-this.currentMonthlyFlow.costoProduccion;
     this.currentMonthlyFlow.costosFijos = this.totalOperatingCosts;
     this.currentMonthlyFlow.utilidadNeta = this.currentMonthlyFlow.utilidadBruta-this.currentMonthlyFlow.costosFijos;
@@ -100,10 +103,11 @@ export class MonthlyFlowComponent implements OnInit {
   calculateJunuary(){
     //this.saveFlowCurrentMonth(this.previousMonthFlow,this.initialMonthFlow);
     this.currentMonthlyFlow.saldoInicial = this.previousMonthFlow.flujoAcumulado;
-    this.currentMonthlyFlow.ingresos = 46000;
-    this.currentMonthlyFlow.costoProduccion = 1359;
-    this.currentMonthlyFlow.utilidadBruta = this.currentMonthlyFlow.ingresos-this.currentMonthlyFlow.costoProduccion;
-    console.log(this.totalOperatingCosts)
+    this.db.getCollection<any>('/Estimaciones/estimicion-1/comportamientoVentas').subscribe((data)=>{
+      this.currentMonthlyFlow.ingresos = data.enero.venta;
+      this.currentMonthlyFlow.costoProduccion = data.enero.costoVenta;
+      this.currentMonthlyFlow.utilidadBruta = this.currentMonthlyFlow.ingresos-this.currentMonthlyFlow.costoProduccion;
+    })
     this.db.getCollection<any>('/Estimaciones/estimicion-1/costos-operativos').subscribe( (data)=>{
       this.currentMonthlyFlow.costosFijos = data.totalCostosOperativos == undefined?0:data.totalCostosOperativos;
       this.currentMonthlyFlow.utilidadNeta = this.currentMonthlyFlow.utilidadBruta-this.currentMonthlyFlow.costosFijos;
@@ -167,7 +171,6 @@ export class MonthlyFlowComponent implements OnInit {
   
           this.planPagosVariado.calPlanPagosFijo(data.montoFinanciar, data.tasaInteres, data.plazo, data.poliza);
           this.cuotas = this.planPagosVariado.cuotasF
-          console.log(this.cuotas)
         }
         if (data.tipoCuota == "Cuota Variable") {
           this.planPagosVariado.calPlanPagosVariado(data.montoFinanciar, data.tasaInteres, data.plazo, data.poliza);
@@ -177,4 +180,34 @@ export class MonthlyFlowComponent implements OnInit {
       }
     })
   }
+  calculateCostOfSale(){
+    this.db.getCollection<any>('/Estimaciones/estimicion-1/comportamientoVentas').subscribe((data)=>{
+      this.costoVenta[0]=data.enero.costoVenta;
+      this.venta[0]=data.enero.venta;
+      this.costoVenta[1]=data.febrero.costoVenta;
+      this.venta[1]=data.febrero.venta;
+      this.costoVenta[2]=data.marzo.costoVenta;
+      this.venta[2]=data.marzo.venta;
+      this.costoVenta[3]=data.abril.costoVenta;
+      this.venta[3]=data.abril.venta;
+      this.costoVenta[4]=data.mayo.costoVenta;
+      this.venta[4]=data.mayo.venta;
+      this.costoVenta[5]=data.junio.costoVenta;
+      this.venta[5]=data.junio.venta;
+      this.costoVenta[6]=data.julio.costoVenta;
+      this.venta[6]=data.julio.venta;
+      this.costoVenta[7]=data.agosto.costoVenta;
+      this.venta[7]=data.agosto.venta;
+      this.costoVenta[8]=data.septiembre.costoVenta;
+      this.venta[8]=data.septiembre.venta;
+      this.costoVenta[9]=data.octubre.costoVenta;
+      this.venta[9]=data.octubre.venta;
+      this.costoVenta[10]=data.noviembre.costoVenta;
+      this.venta[10]=data.noviembre.venta;
+      this.costoVenta[11]=data.diciembre.costoVenta;
+      this.venta[11]=data.diciembre.venta;
+    })
+    
+  }
+
 }
